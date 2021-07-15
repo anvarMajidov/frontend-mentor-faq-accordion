5f93cf7eb8b3cd165f9ad9c723785cce3dc47170
Anvarrich <majidovanvar30.11.2002@gmail.com>
2021-07-15 23:00:21 +0500

    accordion functionality is done, also css and html has been changed a little, image section is not done yet

---
+++ app.js
@@ -0,0 +1,35 @@
+let accordionQuestions = document.querySelectorAll(".accordion__question");
+let accordionAnswers = document.querySelectorAll(".accordion__answer");
+let buttons = document.querySelectorAll(".button");
+
+accordionQuestions.forEach((accQuest, index) => {
+  accQuest.addEventListener("click", (e) => {
+    let panel = accordionAnswers[index];
+    let button = buttons[index];
+
+    if (panel.style.maxHeight) {
+      //accordion is open, let's close it
+      panel.style.maxHeight = null;
+
+      panel.classList.remove("open");
+      accQuest.classList.remove("open");
+      button.classList.remove("active");
+    } else {
+      //accordion is closed, but before opening new one let's close previous accordion
+      //remove className "open"
+      accordionQuestions.forEach((el) => el.classList.remove("open"));
+      accordionAnswers.forEach((el) => {
+        //!important- following piece of code closes all previous accordions
+        el.style.maxHeight = null;
+        el.classList.remove("open");
+      });
+      buttons.forEach((el) => el.classList.remove("active"));
+
+      panel.style.maxHeight = panel.scrollHeight + "px";
+
+      accQuest.classList.add("open");
+      panel.classList.add("open");
+      button.classList.add("active");
+    }
+  });
+});
